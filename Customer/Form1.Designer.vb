daa59a8a9770b0ac8ff6841c63d60f73186206aa
Juned Ahmed <junaidsheikh45@gmail.com>
2025-08-02 20:01:00 +0530

    first push

---
+++ Customer/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnCustomer = New System.Windows.Forms.Button()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.lblWeight = New System.Windows.Forms.Label()
+        Me.lblPurchase = New System.Windows.Forms.Label()
+        Me.lblAddress = New System.Windows.Forms.Label()
+        Me.txtDOB = New System.Windows.Forms.TextBox()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.txtPurchase = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'BtnCustomer
+        '
+        Me.BtnCustomer.Location = New System.Drawing.Point(553, 156)
+        Me.BtnCustomer.Name = "BtnCustomer"
+        Me.BtnCustomer.Size = New System.Drawing.Size(118, 46)
+        Me.BtnCustomer.TabIndex = 0
+        Me.BtnCustomer.Text = "Customer Registration"
+        Me.BtnCustomer.UseVisualStyleBackColor = True
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(95, 36)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(72, 16)
+        Me.lblFirstName.TabIndex = 1
+        Me.lblFirstName.Text = "First Name"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(95, 86)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(72, 16)
+        Me.lblLastName.TabIndex = 2
+        Me.lblLastName.Text = "Last Name"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(198, 36)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(198, 22)
+        Me.txtFirstName.TabIndex = 3
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(198, 86)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(198, 22)
+        Me.txtLastName.TabIndex = 4
+        '
+        'lblDOB
+        '
+        Me.lblDOB.AutoSize = True
+        Me.lblDOB.Location = New System.Drawing.Point(95, 136)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(79, 16)
+        Me.lblDOB.TabIndex = 5
+        Me.lblDOB.Text = "Date of Birth"
+        '
+        'lblWeight
+        '
+        Me.lblWeight.AutoSize = True
+        Me.lblWeight.Location = New System.Drawing.Point(95, 186)
+        Me.lblWeight.Name = "lblWeight"
+        Me.lblWeight.Size = New System.Drawing.Size(49, 16)
+        Me.lblWeight.TabIndex = 6
+        Me.lblWeight.Text = "Weight"
+        '
+        'lblPurchase
+        '
+        Me.lblPurchase.AutoSize = True
+        Me.lblPurchase.Location = New System.Drawing.Point(95, 236)
+        Me.lblPurchase.Name = "lblPurchase"
+        Me.lblPurchase.Size = New System.Drawing.Size(64, 16)
+        Me.lblPurchase.TabIndex = 7
+        Me.lblPurchase.Text = "Purchase"
+        '
+        'lblAddress
+        '
+        Me.lblAddress.AutoSize = True
+        Me.lblAddress.Location = New System.Drawing.Point(95, 286)
+        Me.lblAddress.Name = "lblAddress"
+        Me.lblAddress.Size = New System.Drawing.Size(58, 16)
+        Me.lblAddress.TabIndex = 8
+        Me.lblAddress.Text = "Address"
+        '
+        'txtDOB
+        '
+        Me.txtDOB.Location = New System.Drawing.Point(198, 136)
+        Me.txtDOB.Name = "txtDOB"
+        Me.txtDOB.Size = New System.Drawing.Size(198, 22)
+        Me.txtDOB.TabIndex = 9
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Location = New System.Drawing.Point(198, 186)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(198, 22)
+        Me.txtWeight.TabIndex = 10
+        '
+        'txtPurchase
+        '
+        Me.txtPurchase.Location = New System.Drawing.Point(198, 236)
+        Me.txtPurchase.Name = "txtPurchase"
+        Me.txtPurchase.Size = New System.Drawing.Size(198, 22)
+        Me.txtPurchase.TabIndex = 11
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(198, 286)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(198, 22)
+        Me.txtAddress.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtPurchase)
+        Me.Controls.Add(Me.txtWeight)
+        Me.Controls.Add(Me.txtDOB)
+        Me.Controls.Add(Me.lblAddress)
+        Me.Controls.Add(Me.lblPurchase)
+        Me.Controls.Add(Me.lblWeight)
+        Me.Controls.Add(Me.lblDOB)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.BtnCustomer)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnCustomer As Button
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents lblDOB As Label
+    Friend WithEvents lblWeight As Label
+    Friend WithEvents lblPurchase As Label
+    Friend WithEvents lblAddress As Label
+    Friend WithEvents txtDOB As TextBox
+    Friend WithEvents txtWeight As TextBox
+    Friend WithEvents txtPurchase As TextBox
+    Friend WithEvents txtAddress As TextBox
+End Class
